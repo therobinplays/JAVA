df55aaedc8b56134594c436d2a69ce53ad697526
therobinplays <therealrobinplys>
2025-12-14 15:20:38 +0545

    add

---
+++ Questions/Guess.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner ;
+public class Guess {
+
+    int value;
+
+    public Guess( int value) {
+        this.value = value; 
+        
+    }
+
+    
+
+
+
+    public static void main (String[] args) {
+
+        while {}
+        Scanner scan = new Scanner(System.in);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        scan.close();
+
+    }
+}
